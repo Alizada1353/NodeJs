939a43416855281ae837402a04be8e87635b0029
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-12 13:38:16 +0200

    using process argv to get arguments from terminal

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const path = require('path')
-console.log(`the file name is ${path.basename(__filename)}`);
+// const path = require('path')
+// console.log(`the file name is ${path.basename(__filename)}`);
+
+const grap =  flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+};
+
+const greeting = grap('--greeting');
+const person = grap('--person');
+
+console.log(`${greeting} ${person}`);
