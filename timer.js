16ef81c99eee552c2df4a4504f7cdee3aec0df07
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-12 14:24:32 +0200

    using interval to messure delay time

--- timer.js
+++ timer.js
@@ -1,6 +1,17 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`Waiting ${currentTime / 1000} seconds.`)
+}
+
 console.log(`setting a ${waitTime / 100} seconds delay.`)
 
-const timerFinished = () => console.log('done!');
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log('done!')};
 
+const interval =  setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
